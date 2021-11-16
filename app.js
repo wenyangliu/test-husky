c4ae2ca871d44965e1304271b1be0a27e16f6270
liuxiang <1753439422@qq.com>
2021-11-16 17:27:11 +0800

    test: eslint

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const a = 123
 console.log(a)
 
-// const obj = {m: 'foo', n: 'bar'}
-// const {m: x, n: y} = obj
-// console.log(x, y)
-
-const obj = { m: 'foo', n: 'bar' };
+const obj = {m: 'foo', n: 'bar'}
 const {m: x, n: y} = obj
 console.log(x, y)
+
+const f = function(num) {
+  const sum = a + num
+  return sum
+}
+console.log(f(1))
